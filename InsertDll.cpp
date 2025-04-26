6221e4c456cd0b6b65bc1ea424d2df112409c943
Priya Chanchal <145918494+Priya-C-016@users.noreply.github.com>
2025-04-26 21:54:07 +0530

    InsertDll.cpp

---
+++ InsertDll.cpp
@@ -0,0 +1,15 @@
+void insertAtEnd(Node* &head, int val) {
+    Node* newNode = new Node(val);
+
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+    newNode->prev = temp;
+}
