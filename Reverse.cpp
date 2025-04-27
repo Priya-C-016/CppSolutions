1434eaf2de305beca3a059c9883e8d250f571aac
Priya Chanchal <145918494+Priya-C-016@users.noreply.github.com>
2025-04-27 22:42:58 +0530

    Reverse.cpp

---
+++ Reverse.cpp
@@ -0,0 +1,12 @@
+ ListNode* reverseList(ListNode* head) {
+        if(head==NULL)return NULL;
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        while(curr!=NULL){
+            ListNode* temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        return prev;
+    }
